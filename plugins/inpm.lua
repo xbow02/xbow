1f9920d88bd6472b4369bafdfee4fac8b8fa819b
xbow02 <khederzadehmohama@gmail.com>
2016-09-03 14:25:23 +0430

    Update inpm.lua

--- plugins/inpm.lua
+++ plugins/inpm.lua
@@ -87,7 +87,7 @@ local name_log = user_print_name(msg.from)
 
 	if matches[1] == 'help' and msg.to.type == 'user' or matches[1] == 'pmhelp' and is_admin1(msg) and msg.to.type ~= 'user' then
       	savelog(msg.to.id, name_log.." ["..msg.from.id.."] used pm help")
-		text = "Welcome to Robot!\n\nThis is a bot run by @adminuser for managing the official group @botuser on Telegram\n\nCheck out our channel @channel\n\nAlso join To Group Support\n\nGroupLink for knowing more!"
+		text = "Welcome to Robot!\n\nThis is a bot run by @aryaxbow for managing the official group @xbow09 on Telegram\n\nCheck out our channel @xbow09\n\nAlso join To Group Support\n\nhttps://telegram.me/joinchat/BX_uAAp6uxrAxeJ9JEJG-A for knowing more!"
      	return text
     end
 
